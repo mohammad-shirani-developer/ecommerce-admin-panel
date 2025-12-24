36a8125d91928877f63e5464aedfffaf96db5f26
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-12-24 11:58:48 +0330

    feat: add Vazirmatn Persian font and apply RTL typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,60 @@
+import { Figtree } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const figtree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-figtree",
+});
+
+const vazirmatn = localFont({
+  src: [
+    {
+      path: "../../public/fonts/vazirmatn/Vazirmatn-ExtraLight.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/vazirmatn/Vazirmatn-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/vazirmatn/Vazirmatn-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/vazirmatn/Vazirmatn-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/vazirmatn/Vazirmatn-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/vazirmatn/Vazirmatn-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/vazirmatn/Vazirmatn-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/vazirmatn/Vazirmatn-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-vazirmantn",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +62,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html dir="rtl" className="dark">
+    <html
+      dir="rtl"
+      className={` dark ${vazirmatn.variable} ${figtree.variable}`}
+    >
       <body className=" bg-gray-900 text-gray-100">{children}</body>
     </html>
   );
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,9 @@
 const Sidebar = () => {
   return (
     <aside className="w-64 bg-gray-800 min-h-screen p-4">
-      <h1 className="text-xl font-bold mb-6">E-Commerce Admin</h1>
+      <h1 className="text-xl font-bold mb-6" lang="en">
+        E-Commerce Admin
+      </h1>
       <nav className="space-y-2">
         <a className="block rounded hover:bg-gray-700">داشبور</a>
         <a className="block rounded hover:bg-gray-700">کاربران</a>
