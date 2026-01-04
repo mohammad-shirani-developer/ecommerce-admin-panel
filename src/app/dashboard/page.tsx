b7ae9e075330c8d4115ce8930d15be18600b9f11
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2026-01-04 17:25:26 +0330

    feat(dashboard): integrate product sales bar chart and calculate sales data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import ProductSalesBarChart from "@/components/dashboard/ProductSalesBarChart";
+import { calculateSalesByCategory } from "@/utils/calculateSalesByCategory";
 import { getDashboardStats } from "@/utils/dashboardData";
 import { generateProducts } from "@/utils/generateProducts";
 import { generateUsers } from "@/utils/generateUsers";
@@ -7,12 +9,16 @@ import StatsGrid from "./StatsGrid";
 const DashboardPage = () => {
   const users = generateUsers(20);
   const products = generateProducts(15);
+  const salesData = calculateSalesByCategory(products);
   const stats = getDashboardStats(users, products);
 
   return (
     <>
-      <h2 className="text-2xl font-bold mb-4">داشبورد</h2>
-      <StatsGrid stats={stats} />
+      <div className="space-y-6">
+        <h2 className="text-2xl font-bold mb-4">داشبورد</h2>
+        <StatsGrid stats={stats} />
+        <ProductSalesBarChart data={salesData} />
+      </div>
     </>
   );
 };
