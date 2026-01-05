1e143ee55a8cea0ec1e0f60127bd9ac362aab755
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2026-01-05 11:01:37 +0330

    feat(dashboard): refactor chart imports and adapt data calculations for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import ProductSalesBarChart from "@/components/dashboard/ProductSalesBarChart";
-import ProductStatusPieChart from "@/components/dashboard/ProductStatusPieChart";
-import { calculateProductStatusStats } from "@/utils/calculateProductStatusStats";
-import { calculateSalesByCategory } from "@/utils/calculateSalesByCategory";
+
+import ProductSalesBarChart from "@/components/charts/ProductSalesBarChart";
+import ProductStatusPieChart from "@/components/charts/ProductStatusPieChart";
+import { adaptProductStatusToChart } from "@/utils/charts/productStatus.adapter";
+import { adaptProductsToSalesChart } from "@/utils/charts/salesBar.adapter";
 import { getDashboardStats } from "@/utils/dashboardData";
 import { generateProducts } from "@/utils/generateProducts";
 import { generateUsers } from "@/utils/generateUsers";
@@ -11,9 +12,10 @@ import StatsGrid from "./StatsGrid";
 const DashboardPage = () => {
   const users = generateUsers(20);
   const products = generateProducts(15);
-  const salesData = calculateSalesByCategory(products);
   const stats = getDashboardStats(users, products);
-  const statusData = calculateProductStatusStats(products);
+  const statusData = adaptProductStatusToChart(products);
+  const salesData = adaptProductsToSalesChart(products);
+
   return (
     <>
       <div className="space-y-6">
